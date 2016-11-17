3f457c9b7a3b06bdc7b798644693ef2a4ca797a6
Lucas Kramer <krame505@umn.edu>
2016-11-17 13:07:40 -0600

    Add constructor for Moves

--- state.hpp
+++ state.hpp
@@ -63,8 +63,7 @@ struct BoardItem {
 #ifdef __CUDACC__
   __host__ __device__
 #endif
-  BoardItem() {}
-  
+  BoardItem() {}  
 };
 
 struct Move;
@@ -130,9 +129,21 @@ struct Move {
   Loc to;
   Loc removed[MAX_MOVE_JUMPS];
   Loc intermediate[MAX_MOVE_JUMPS];
+  uint8_t jumps;
   bool promoted;
   PieceType newType; // Type after the move - same as original type if no promotion
-  uint8_t jumps;
+
+#ifdef __CUDACC__
+  __host__ __device__
+#endif
+  Move(Loc from, Loc to, uint8_t jumps, bool promoted=false, PieceType newType=CHECKER) :
+    from(from), to(to), jumps(jumps), promoted(promoted), newType(newType) {}
+
+  // Default constructor must be empty to avoid a race condition when initializing shared memory
+#ifdef __CUDACC__
+  __host__ __device__
+#endif
+  Move() {}
 
   // Return true if making this move prevents the other move from being made
 #ifdef __CUDACC__
