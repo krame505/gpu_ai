662261eb21360702acd430be1122d5d8f0477291
Lucas Kramer <krame505@umn.edu>
2016-12-09 14:03:48 -0600

    Switching to OpenMP for host playout parallelization

--- src/playout.cpp
+++ src/playout.cpp
@@ -3,43 +3,23 @@
 #include "player.hpp"
 
 #include <vector>
-#include <future>
 #include <cassert>
 using namespace std;
 
 #define NUM_THREADS 8
-
-vector<PlayerId> worker(const vector<State> &states) {
+vector<PlayerId> hostPlayouts(vector<State> states) {
+  vector<PlayerId> results(states.size());
   RandomPlayer player;
-  vector<PlayerId> results;
-  for (State state : states) {
+
+  #pragma omp parallel for
+  for (unsigned i = 0; i < states.size(); i++) {
+    State state = states[i];
     while (!state.isFinished()) {
       Move move = player.getMove(state);
       state.move(move);
     }
-    results.push_back(state.result());
-  }
-  return results;
-}
-
-vector<PlayerId> hostPlayouts(vector<State> states) {
-  unsigned blockSize = states.size() / NUM_THREADS;
-  vector<future<vector<PlayerId>>> results;
-
-  for (auto it = states.begin(); it < states.end(); it += blockSize) {
-    vector<State> threadStates;
-    if (it + blockSize < states.end())
-      threadStates = vector<State>(it, it + blockSize);
-    else
-      threadStates = vector<State>(it, states.end());
-    results.push_back(async(launch::async, worker, threadStates));
-  }
- 
-  vector<PlayerId> result;
-  for (future<vector<PlayerId>> &res : results) {
-    vector<PlayerId> threadResult = res.get();
-    result.insert(result.end(), threadResult.begin(), threadResult.end());
+    results[i] = state.result();
   }
   
-  return result;
+  return results;
 }
