9acef4594fac2283d25de973d434c08a7a4ac775
Lucas Kramer <krame505@umn.edu>
2016-12-29 11:55:09 -0600

    Cleanup, added test driver to play games between 2 players, added checking for draws in playGame

--- src/driver.cpp
+++ src/driver.cpp
@@ -19,15 +19,20 @@
 
 #endif
 
+#define DEFAULT_NUM_GAMES 10
+
 #define NUM_TEST_SETUPS 2
 #define SEED 2016
 #define MAX_RANDOM_MOVES 100 // NOTE : Is 100 max random moves reasonable?  How long is an average checkers game?
+
+#define NUM_DRAW_MOVES 50
 
 using namespace std;
 
 enum runMode {
   GenMovesTest,
-  Test,
+  PlayoutTest,
+  GameTest,
   Game
 };
 
@@ -37,8 +42,11 @@ istream &operator>>(istream& in, runMode& mode) {
   if (token == "gen_moves_test") {
     mode = GenMovesTest;
   }
-  else if (token == "test") {
-    mode = Test;
+  else if (token == "playout_test") {
+    mode = PlayoutTest;
+  }
+  else if (token == "game_test") {
+    mode = GameTest;
   }
   else if (token == "game") {
     mode = Game;
@@ -54,55 +62,15 @@ ostream &operator<<(ostream &os, runMode mode) {
   switch (mode) {
   case GenMovesTest:
     return os << "gen_moves_test";
-  case Test:
-    return os << "test";
+  case PlayoutTest:
+    return os << "playout_test";
+  case GameTest:
+    return os << "game_test";
   case Game:
     return os << "game";
   default:
     throw runtime_error("Unknown run mode");
   }
-}
-
-
-// playGame: Implements the game logic.
-// Inputs
-// players: Array of pointers to Player classes (human, random, or MCTS)
-// verbose (optional): Enable verbose output.
-// Return: The PlayerId of the winning player (or PLAYER_NONE if there is a draw)
-PlayerId playGame(Player *players[NUM_PLAYERS], bool verbose=true) {
-  State state = getStartingState();
-
-  // Game is over when there are no more possible moves
-  while (!state.isGameOver()) {
-    if (verbose) {
-      cout << state << endl;
-    }
-    
-    // Get which player's turn it is from the state
-    Player *player = players[state.turn];
-    // Calculate the next move
-    Move move = player->getMove(state);
-    cout << state.turn << " moved " << move << endl;
-    // Apply that move to the state
-    state.move(move);
-    cout << endl;
-  }
-
-  if (verbose) {
-    cout << state << endl;
-  }
-
-  // Game over: Other player is the winner
-  PlayerId result = state.getNextTurn();
-  if (verbose) {
-    if (result == PLAYER_NONE) {
-      cout << "Draw!" << endl;
-    }
-    else {
-      cout << result << " won!" << endl;
-    }
-  }
-  return result;
 }
 
 vector<State> genRandomStates(unsigned int numTests) {
@@ -135,6 +103,19 @@ vector<State> genRandomStates(unsigned int numTests) {
   return ourStates;
 }
 
+void genMovesTests(unsigned int numTests) {
+  vector<State> states = genRandomStates(numTests);
+  
+  cout << "Testing genMoves host and device results are the same..." << endl;
+  for (State state : states) {
+    if (!genMovesTest(state)) {
+      cout << "Failed" << endl;
+      exit(1);
+    }
+  }
+  cout << "Passed" << endl;
+}
+
 vector<PlayerId> playoutTest(const vector<State> &states, PlayoutDriver *playoutDriver) {
   auto t1 = chrono::high_resolution_clock::now();
   vector<PlayerId> playoutResults = playoutDriver->runPlayouts(states);
@@ -165,31 +146,104 @@ vector<PlayerId> playoutTest(const vector<State> &states, PlayoutDriver *playout
   return playoutResults;
 }
 
-void playoutTests(unsigned int numTests, PlayoutDriver *playoutDrivers[NUM_TEST_SETUPS]) {
+void playoutTests(unsigned numTests, PlayoutDriver *playoutDrivers[NUM_TEST_SETUPS]) {
   vector<State> states = genRandomStates(numTests);
   
   // Needed to avoid extra overhead of first kernel call
   cout << "Initializing CUDA context..." << endl;
   cudaDeviceSynchronize();
 
-  for (unsigned int i = 0; i < NUM_TEST_SETUPS; i++) {
+  for (unsigned i = 0; i < NUM_TEST_SETUPS; i++) {
     cout << "Running test " << i << ": " << playoutDrivers[i]->getName() << "..." << endl;
     playoutTest(states, playoutDrivers[i]);
     cout << endl;
   }
 }
 
-void genMovesTests(unsigned int numTests) {
-  vector<State> states = genRandomStates(numTests);
-  
-  cout << "Testing genMoves host and device results are the same..." << endl;
-  for (State state : states) {
-    if (!genMovesTest(state)) {
-      cout << "Failed" << endl;
-      exit(1);
+// playGame: Implements the game logic.
+// Inputs
+// players: Array of pointers to Player classes (human, random, or MCTS)
+// verbose (optional): Enable verbose output.
+// Return: The PlayerId of the winning player (or PLAYER_NONE if there is a draw)
+PlayerId playGame(Player *players[NUM_PLAYERS], bool verbose=true) {
+  State state = getStartingState();
+  unsigned movesSinceLastCapture = 0;
+
+  // Game is over when there are no more possible moves or no captures have been made for an extended time
+  while (!state.isGameOver() && movesSinceLastCapture < NUM_DRAW_MOVES) {
+    if (verbose) {
+      cout << state << endl;
+    }
+    
+    // Get which player's turn it is from the state
+    Player *player = players[state.turn];
+
+    // Calculate the next move
+    Move move = player->getMove(state, verbose);
+    if (verbose) {
+      cout << state.turn << " moved " << move << endl;
+    }
+
+    // Update the number of moves since the last capture
+    if (move.jumps > 0)
+      movesSinceLastCapture = 0;
+    else
+      movesSinceLastCapture++;
+
+    // Apply that move to the state
+    state.move(move);
+    if (verbose) {
+      cout << endl;
     }
   }
-  cout << "Passed" << endl;
+
+  if (verbose) {
+    cout << state << endl;
+  }
+
+  // Game over: Other player is the winner, unless the game was a draw
+  PlayerId result = movesSinceLastCapture < NUM_DRAW_MOVES? state.getNextTurn() : PLAYER_NONE;
+  if (verbose) {
+    if (result == PLAYER_NONE) {
+      cout << "Draw!" << endl;
+    }
+    else {
+      cout << result << " won!" << endl;
+    }
+  }
+  return result;
+}
+
+void gameTests(unsigned numTests, Player *players[NUM_PLAYERS]) {
+  int wins[3] = {0, 0, 0};
+  for (unsigned i = 0; i < numTests; i++) {
+    cout << "Playing game " << i << "... ";
+    PlayerId result = playGame(players, false);
+
+    if (result == PLAYER_NONE) {
+      cout << "Draw!" << endl;
+    }
+    else {
+      cout << result << " won!" << endl;
+    }
+
+    switch (result) {
+    case PLAYER_NONE:
+      wins[0]++;
+      break;
+    case PLAYER_1:
+      wins[1]++;
+      break;
+    case PLAYER_2:
+      wins[2]++;
+      break;
+    }
+  }
+
+  cout << "=== Results ===" << endl;
+  cout << "Games drawn: " << wins[0] << endl;
+  cout << "Player 1 wins: " << wins[1] << endl;
+  cout << "Player 2 wins: " << wins[2] << endl;
 }
 
 int main(int argc, char **argv) {
@@ -206,10 +260,10 @@ int main(int argc, char **argv) {
 
   boost::program_options::options_description desc("Allowed options");
   desc.add_options()
-    ("mode,m", boost::program_options::value<runMode>(&theRunMode)->default_value(Game), "run mode\ngame test gen_moves_test")
-    ("num-playouts,n", boost::program_options::value<unsigned int>(&numTests)->default_value(DEFAULT_NUM_PLAYOUTS), "number of playouts")
-    ("player1,1", boost::program_options::value<string>(), "player 1 (mode = game)\nhuman random mcts mcts_host mcts_device_single mcts_device_heuristic mcts_device_multiple mcts_device_coarse mcts_hybrid mcts_optimal\ntest 1 (mode = test)\nhost device_single device_heuristic device_multiple device_coarse hybrid optimal")
-    ("player2,2", boost::program_options::value<string>(), "player 2 (mode = game)\ntest 2 (mode = test)")
+    ("mode,m", boost::program_options::value<runMode>(&theRunMode)->default_value(Game), "run mode\n gen_moves_test playout_test game_test game")
+    ("num-playouts,n", boost::program_options::value<unsigned int>(&numTests), "number of playouts")
+    ("player1,1", boost::program_options::value<string>(), "player 1 (mode = game, game_test)\nhuman random mcts mcts_host mcts_device_multiple mcts_device_coarse mcts_hybrid mcts_optimal\ntest 1 (mode = playout_test)\nhost device_single device_heuristic device_multiple device_coarse hybrid optimal")
+    ("player2,2", boost::program_options::value<string>(), "player 2 (mode = game, game_test)\ntest 2 (mode = playout_test)")
     ("help,h", "print help")
     ;
   boost::program_options::variables_map vm;
@@ -218,11 +272,20 @@ int main(int argc, char **argv) {
     boost::program_options::notify(vm);
     if (vm.count("help")) {
       cout << desc << endl;
-      return 1;
+      return 0;
+    }
+
+    if (!vm.count("num-playouts")) {
+      if (theRunMode == GameTest) {
+	numTests = DEFAULT_NUM_GAMES;
+      }
+      else {
+	numTests = DEFAULT_NUM_PLAYOUTS;
+      }
     }
   
     if (vm.count("player1")) {
-      if (theRunMode == Game) {
+      if (theRunMode == Game || theRunMode == GameTest) {
         player1 = getPlayer(vm["player1"].as<string>());
       }
       else {
@@ -230,7 +293,7 @@ int main(int argc, char **argv) {
       }
     }
     else {
-      if (theRunMode == Game) {
+      if (theRunMode == Game || theRunMode == GameTest) {
         player1 = new HumanPlayer;
       }
       else {
@@ -239,7 +302,7 @@ int main(int argc, char **argv) {
     }    
 
     if (vm.count("player2")) {
-      if (theRunMode == Game) {
+      if (theRunMode == Game || theRunMode == GameTest) {
         player2 = getPlayer(vm["player2"].as<string>());
       }
       else {
@@ -247,7 +310,7 @@ int main(int argc, char **argv) {
       }
     }
     else {
-      if (theRunMode == Game) {
+      if (theRunMode == Game || theRunMode == GameTest) {
         player2 = new MCTSPlayer;
       }
       else {
@@ -263,36 +326,61 @@ int main(int argc, char **argv) {
 
 
   // Run the game
-  if (theRunMode == Game) {
-    cout << "Playing single game of ";
-    cout << player1->getName() << " Player 1 and ";
-    cout << player2->getName() << " Player 2" << endl;
+  switch (theRunMode) {
+  case GenMovesTest:
+    cout << "Testing " << numTests << " random states have the same moves on the host and device" << endl;
+    cout << endl;
 
-    Player *players[NUM_PLAYERS] = {player1, player2};
-    playGame(players);
+    genMovesTests(numTests);
+    break;
 
-    // Free players as we are done now
-    delete player1;
-    delete player2;
-  }
-  else if (theRunMode == Test) {
+  case PlayoutTest:
     cout << "Running " << numTests << " random playouts with ";
     cout << playoutDriver1->getName() << " and ";
     cout << playoutDriver2->getName() << endl;
     cout << endl;
 
-    PlayoutDriver *playoutDrivers[NUM_TEST_SETUPS] = {playoutDriver1, playoutDriver2};
-    playoutTests(numTests, playoutDrivers);
+    {
+      PlayoutDriver *playoutDrivers[NUM_TEST_SETUPS] = {playoutDriver1, playoutDriver2};
+      playoutTests(numTests, playoutDrivers);
+    }
 
     // Free playout drivers as we are done now
     delete playoutDriver1;
     delete playoutDriver2;
-  }
-  else if (theRunMode == GenMovesTest) {
-    cout << "Testing " << numTests << " random states have the same moves on the host and device" << endl;
+    break;
+
+  case GameTest:
+    cout << "Playing " << numTests << " games with ";
+    cout << player1->getName() << " Player 1 and ";
+    cout << player2->getName() << " Player 2" << endl;
     cout << endl;
 
-    genMovesTests(numTests);
+    {
+      Player *players[NUM_PLAYERS] = {player1, player2};
+      gameTests(numTests, players);
+    }
+
+    // Free players as we are done now
+    delete player1;
+    delete player2;
+    
+    break;
+
+  case Game:
+    cout << "Playing single game of ";
+    cout << player1->getName() << " Player 1 and ";
+    cout << player2->getName() << " Player 2" << endl;
+
+    {
+      Player *players[NUM_PLAYERS] = {player1, player2};
+      playGame(players);
+    }
+
+    // Free players as we are done now
+    delete player1;
+    delete player2;
+    break;
   }
 
   return 0;
